9e0fd92389d8f40369d0700f49092cd6a522cd52
Serge Meunier <sjmeunier@gmail.com>
2016-11-03 13:25:26 +0100

    Fixed stupid mistake

--- static/js/services.js
+++ static/js/services.js
@@ -1,5 +1,5 @@
 app.factory('socket', function ($rootScope) {
-  var socket = io.connect(), {reconnect: true});
+  var socket = io.connect();
   return {
       joinRoom: function(roomName, playerName, isObserver) {
         socket.emit('joinRoom', roomName, playerName, isObserver);
