e79a104788c13dbae9566e1836e0a95fe2066739
Serge Meunier <sjmeunier@gmail.com>
2016-11-03 10:49:36 +0100

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -3,7 +3,7 @@ define([], function() {
 
     var config = {};
 
-    config.serverUrl = 'http://10.210.0.242:5050';
+    config.serverUrl = 'https://nodejs-planning-poker.herokuapp.com:5050';
     config.pointValues = [1, 2, 3, 5, 8, 13, 20, 40, 100, '?', '∞', '🍵'];
     config.nameCookieId = 'name';
     config.roomCookieId = 'room';
